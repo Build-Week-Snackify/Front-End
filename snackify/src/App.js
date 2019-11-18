34164df03cf568ac867d593e39e2f0eecdb0edd2
Memitaru <amandatwah@gmail.com>
2019-11-18 11:56:33 -0800

    Fixed package.json to include necessary dependencies

--- snackify/src/App.js
+++ snackify/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import HomePage from './Components/HomePage';
