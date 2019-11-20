eddd28575a462c699e6c33edef6df6f8fddfef4d
Roberto Banbanaste <rbanbanaste@outlook.com>
2019-11-20 14:24:33 -0500

    added styles to NavBar and RegisterEmp

--- snackify/src/Components/NavBar.js
+++ snackify/src/Components/NavBar.js
@@ -1,38 +1,53 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 import { connect } from "react-redux";
 
-
 const Navigation = styled.div`
-display: flex;
-justify-content: space-evenly;
-background-color: #00a650;
-color: white;
-height:40px;
-align-items: center;
-`
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #fff;
+  height: 100px;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 
-
+const linkStyle = {
+  color: "#fff",
+  fontSize: "15px",
+  borderRadius: "50px",
+  padding: "5px 20px",
+  backgroundColor: "#ff631b"
+};
 
 export default function NavBar() {
-    if (localStorage.getItem('token')) {
-        return(
-            <Navigation className='NavLink div'>
-                <NavLink to="/">NOT Home</NavLink>
-                <NavLink to="/CompanyOrEmployee">NOT Login</NavLink>
-                <NavLink onClick={() => localStorage.removeItem('token')}  to="/">Sign Out</NavLink>
-            </Navigation>
-        )
-    }
+  if (localStorage.getItem("token")) {
+    return (
+      <Navigation className="NavLink div">
+        <NavLink to="/" style={linkStyle}>
+          NOT Home
+        </NavLink>
+        <NavLink to="/CompanyOrEmployee" style={linkStyle}>
+          NOT Login
+        </NavLink>
+        <NavLink
+          onClick={() => localStorage.removeItem("token")}
+          to="/"
+          style={linkStyle}
+        >
+          Sign Out
+        </NavLink>
+      </Navigation>
+    );
+  }
 
-    if (!localStorage.getItem('token')) {
-        return (
-            <Navigation className='NavLink div'>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/CompanyOrEmployee">Login</NavLink>
-                <NavLink to="/Register">Sign Up</NavLink>
-            </Navigation>
-        )
-    }
+  if (!localStorage.getItem("token")) {
+    return (
+      <Navigation className="NavLink div">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/CompanyOrEmployee">Login</NavLink>
+        <NavLink to="/Register">Sign Up</NavLink>
+      </Navigation>
+    );
+  }
 }
--- snackify/src/Components/RegisterEmp.js
+++ snackify/src/Components/RegisterEmp.js
@@ -1,91 +1,202 @@
-import React, {useState, useEffect} from 'react';
-import {withFormik, Form, Field} from "formik";
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-
-const RegisterFormEmp = ({values, errors, status }) => {
-  const[user, setUser] = useState([]);
+const RegisterFormEmp = ({ values, errors, status }) => {
+  const [user, setUser] = useState([]);
   useEffect(() => {
     status && setUser(user => [...user, status]);
-  },[]);//ends useEffect
+  }, []); //ends useEffect
 
+  const style = {
+    margin: "0 auto",
+    padding: "30px 40px",
+    borderRadius: "5px",
+    boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+    width: "500px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    textAlign: "left"
+  };
+
+  const fieldStyle = {
+    width: "300px",
+    borderRadius: "5px",
+    border: "2px solid rgba(0,0,0,.2)",
+    padding: "2px 0px 0px 10px"
+  };
+
+  const divStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start"
+  };
 
   return (
     <div className="register-form">
-    <Form>
-      <div>
-        <label>User Name
-        <Field type="text" name="username" placeholder="User Name" />
-        <div>{errors.username}</div>
-        </label>
-      </div>
-      <div>
-        <label>Password
-        <Field type="password" name="password" placeholder="minimum 6 characters" />
-        <div>{errors.password}</div>
-        </label>
-      </div>
-      <div>
-        <label>Employee Name
-        <Field type="text" name="fullName" placeholder="Full Name" />
-        <div>{errors.fullName}</div>
-        </label>
-      </div>
-      <div>
-        <label>Personal email
-        <Field type="text" name="contactPerson" placeholder="mypersonal@email.com" />
-        <div>{errors.contactPerson}</div>
-        </label>
-      </div>
-      <div>
-        <label>Organization Name
-        <Field type="text" name="role" placeholder="Organization Name" />
-        <div>{errors.role}</div>
-        </label>
-      </div>
-      <div>
-        <label>Organization ID # (sign-up code)
-        <Field type="text" name="orgId" placeholder="Organization ID" />
-        <div>{errors.orgId}</div>
-        </label>
-      </div>
-      <div>
-        <label>Work Email
-        <Field type="email" name="email" placeholder="email@work.net" />
-        <div>{errors.email}</div>
-        </label>
-      </div>
-      <div>
-        <label>Phone Number
-        <Field type="text" name="phoneNumber" placeholder="(000) 000-0000" />
-        <div>{errors.phoneNumber}</div>
-        </label>
-      </div>
-      <div>
-        <label>Shipping Address, City
-        <Field type="text" name="streetAddress" placeholder="1234 Main St, City" />
-        <div>{errors.streetAddress}</div>
-        </label>
-        <label>State
-        <Field type="text" name="state" placeholder="AA" />
-        <div>{errors.state}</div>
-        </label>
-        <label>Zip Code
-        <Field type="text" name="zipcode" placeholder= "12345" />
-        <div>{errors.zipcode}</div>
-        </label>
-      </div>     
-            
-      <button type="submit">Register Now</button>
-    </Form>
-    {}
-  </div>  
+      <Form style={style}>
+        <div>
+          <label>
+            User Name <br />
+            <Field
+              type="text"
+              name="username"
+              placeholder="User Name"
+              style={fieldStyle}
+            />
+            <div>{errors.username}</div>
+          </label>
+        </div>
+        <div>
+          <label>
+            Password
+            <br />
+            <Field
+              type="password"
+              name="password"
+              placeholder="minimum 6 characters"
+              style={fieldStyle}
+            />
+            <div>{errors.password}</div>
+          </label>
+        </div>
+        <div>
+          <label>
+            Employee Name
+            <br />
+            <Field
+              type="text"
+              name="fullName"
+              placeholder="Full Name"
+              style={fieldStyle}
+            />
+            <div>{errors.fullName}</div>
+          </label>
+        </div>
+        <div>
+          <label>
+            Personal email
+            <br />
+            <Field
+              type="text"
+              name="contactPerson"
+              placeholder="mypersonal@email.com"
+              style={fieldStyle}
+            />
+            <div>{errors.contactPerson}</div>
+          </label>
+        </div>
+        <div>
+          <label>
+            Organization Name
+            <br />
+            <Field
+              type="text"
+              name="role"
+              placeholder="Organization Name"
+              style={fieldStyle}
+            />
+            <div>{errors.role}</div>
+          </label>
+        </div>
+        <div>
+          <label>
+            Organization ID # (sign-up code)
+            <br />
+            <Field
+              type="text"
+              name="orgId"
+              placeholder="Organization ID"
+              style={fieldStyle}
+            />
+            <div>{errors.orgId}</div>
+          </label>
+        </div>
+        <div>
+          <label>
+            Work Email
+            <br />
+            <Field
+              type="email"
+              name="email"
+              placeholder="email@work.net"
+              style={fieldStyle}
+            />
+            <div>{errors.email}</div>
+          </label>
+        </div>
+        <div>
+          <label>
+            Phone Number
+            <br />
+            <Field
+              type="text"
+              name="phoneNumber"
+              placeholder="(000) 000-0000"
+              style={fieldStyle}
+            />
+            <div>{errors.phoneNumber}</div>
+          </label>
+        </div>
+        <div style={divStyle}>
+          <label>
+            Shipping Address
+            <br />
+            <Field
+              type="text"
+              name="streetAddress"
+              placeholder="1234 Main St, City"
+              style={fieldStyle}
+            />
+            <div>{errors.streetAddress}</div>
+          </label>
+          <label>
+            State
+            <br />
+            <Field
+              type="text"
+              name="state"
+              placeholder="AA"
+              style={fieldStyle}
+            />
+            <div>{errors.state}</div>
+          </label>
+          <label>
+            Zip Code
+            <br />
+            <Field
+              type="text"
+              name="zipcode"
+              placeholder="12345"
+              style={fieldStyle}
+            />
+            <div>{errors.zipcode}</div>
+          </label>
+        </div>
+
+        <button type="submit">Register Now</button>
+      </Form>
+      {}
+    </div>
   );
 };
 const FormikRegisterFormEmp = withFormik({
-  mapPropsToValues({username, password, fullName, contactPerson, role, orgId, email, phoneNumber, streetAddress, state, zipcode }){
-    return{
+  mapPropsToValues({
+    username,
+    password,
+    fullName,
+    contactPerson,
+    role,
+    orgId,
+    email,
+    phoneNumber,
+    streetAddress,
+    state,
+    zipcode
+  }) {
+    return {
       username: username || "",
       password: password || "",
       fullName: fullName || "",
@@ -96,9 +207,9 @@ const FormikRegisterFormEmp = withFormik({
       phoneNumber: phoneNumber || "",
       streetAddress: streetAddress || "",
       state: state || "",
-      zipcode: zipcode || "",
-    };//ends return
-  },//ends mapProps
+      zipcode: zipcode || ""
+    }; //ends return
+  }, //ends mapProps
   validationSchema: Yup.object().shape({
     username: Yup.string().required("Please select user name"),
     password: Yup.string().required("Password requires 6 characters"),
@@ -111,18 +222,17 @@ const FormikRegisterFormEmp = withFormik({
     streetAddress: Yup.string().required("Please enter valid address"),
     city: Yup.string().required("City Required"),
     state: Yup.string().required("Two letter state"),
-    zipcode: Yup.string().required("Valid zip code required"),
-  }),//ends validation
-  handleSubmit(values, {setStatus}) {
+    zipcode: Yup.string().required("Valid zip code required")
+  }), //ends validation
+  handleSubmit(values, { setStatus }) {
     axios
-    .post("https://snackify7.herokuapp.com/auth/register/employee", values)
-    .then(response => {
-      setStatus(response.data);
-      console.log(response);
-    })
-    .catch(err => console.log(err.response));
+      .post("https://snackify7.herokuapp.com/auth/register/employee", values)
+      .then(response => {
+        setStatus(response.data);
+        console.log(response);
+      })
+      .catch(err => console.log(err.response));
   }
 })(RegisterFormEmp);
 
 export default FormikRegisterFormEmp;
-
